a0de4e8a4815ad4b847ccc64174b96e510633f3f
Chiman <chimasong@gmail.com>
2021-11-15 16:16:00 +0900

    api request manager

--- Module/API/ApiManager.swift
+++ Module/API/ApiManager.swift
@@ -7,10 +7,56 @@
 //
 
 import Foundation
+import Alamofire
 
 class ApiRequestManager {
     static let shared = ApiRequestManager()
+    let host: URL = URL(string: "https://ek7b8b8yq2.execute-api.us-east-2.amazonaws.com")!
     
-    let host: String = "https://ek7b8b8yq2.execute-api.us-east-2.amazonaws.com"
-    
+    var manager: Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.headers = .default
+        configuration.timeoutIntervalForRequest = 30.0
+        let session = Session(configuration: configuration)
+        return session
+    }()
+}
+
+extension ApiRequestManager {
+    enum Router {
+        case dutchPayList
+        
+        var httpMethod: Alamofire.HTTPMethod {
+            switch self {
+            case .dutchPayList:
+                return .get
+            }
+        }
+        
+        var baseUrl: URL {
+            switch self {
+            case .dutchPayList:
+                return ApiRequestManager.shared.host
+            }
+        }
+        
+        var home: String {
+            switch self {
+            case .dutchPayList:
+                return "/default"
+            }
+        }
+        
+        var path: String {
+            switch self {
+            case .dutchPayList:
+                return "/toss_ios_homework_dutch_detail"
+            }
+        }
+        
+        var url: URL {
+            let resultUrl = baseUrl.appendingPathComponent(path)
+            return resultUrl
+        }
+    }
 }
